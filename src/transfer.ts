5e4732ca31ae3c5112dbb022e28f0347710ec3b4
cathe-zhang <zhangle_media@hotmail.com>
2019-11-01 00:05:37 +0800

    refactor: ts重构

--- src/transfer.ts
+++ src/transfer.ts
@@ -8,18 +8,18 @@
  * @param lng 经度
  * @param lat 纬度
  */
-function transBMapToTMap(lng, lat) {
-	let B_PI = 3.14159265358979324 * 3000.0 / 180.0;
-	let x = lng - 0.0065;
-	let y = lat - 0.006;
-	let z = Math.sqrt(x * x + y * y) - 0.00002 * Math.sin(y * B_PI);
-	let theta = Math.atan2(y, x) - 0.000003 * Math.cos(x * B_PI);
-	let longitude = z * Math.cos(theta);
-	let latitude = z * Math.sin(theta);
-	return {
-		lng: longitude,
-		lat: latitude
-	};
+function transBMapToTMap(lng: number, lat: number) {
+  let B_PI = 3.14159265358979324 * 3000.0 / 180.0;
+  let x = lng - 0.0065;
+  let y = lat - 0.006;
+  let z = Math.sqrt(x * x + y * y) - 0.00002 * Math.sin(y * B_PI);
+  let theta = Math.atan2(y, x) - 0.000003 * Math.cos(x * B_PI);
+  let longitude = z * Math.cos(theta);
+  let latitude = z * Math.sin(theta);
+  return {
+    lng: longitude,
+    lat: latitude
+  };
 };
 
 /**
@@ -27,18 +27,25 @@ function transBMapToTMap(lng, lat) {
  * @param lng 经度
  * @param lat 纬度
  */
-function transTMapToBMap(lng, lat) {
-	let B_PI = 3.14159265358979324 * 3000.0 / 180.0;
-	let x = lng;
-	let y = lat;
-	let z = Math.sqrt(x * x + y * y) + 0.00002 * Math.sin(y * B_PI);
-	let theta = Math.atan2(y, x) + 0.000003 * Math.cos(x * B_PI);
-	let longitude = z * Math.cos(theta) + 0.0065
-	let latitude = z * Math.sin(theta) + 0.006;
-	return {
-		lng: longitude,
-		lat: latitude
-	}
+function transTMapToBMap(lng: number, lat: number) {
+  let B_PI = 3.14159265358979324 * 3000.0 / 180.0;
+  let x = lng;
+  let y = lat;
+  let z = Math.sqrt(x * x + y * y) + 0.00002 * Math.sin(y * B_PI);
+  let theta = Math.atan2(y, x) + 0.000003 * Math.cos(x * B_PI);
+  let longitude = z * Math.cos(theta) + 0.0065
+  let latitude = z * Math.sin(theta) + 0.006;
+  return {
+    lng: longitude,
+    lat: latitude
+  }
+}
+
+interface CoordinateParams {
+  from: 'bmap' | 'gmap' | 'tmap',
+  to: 'bmap' | 'gmap' | 'tmap',
+  lng: number;
+  lat: number;
 }
 
 /**
@@ -46,37 +53,37 @@ function transTMapToBMap(lng, lat) {
  * @params 参数对象
  * @return 是否通过验证 boolean
  */
-function verifyParams(params) {
+function verifyParams(params: CoordinateParams) {
   const { from, to, lng, lat } = params
   // 参数存在验证
   Object.keys(paramerrorings).forEach(element => {
-    if ( !params[element] ) {
+    if (!params[element]) {
       throwError(paramerrorings[element]);
       return false
     }
   });
 
   // 经纬度类型验证
-  if ( typeof lng !== 'number' && typeof lat !== 'number' ) {
+  if (typeof lng !== 'number' && typeof lat !== 'number') {
     throwError('传入的经纬度必须是number类型，请检查您的参数。')
     return false
-  } 
+  }
 
   // 参数值合法验证
-  if ( !['bmap', 'tmap', 'gmap'].includes(from) || !['bmap', 'tmap', 'gmap'].includes(to) ) {
+  if (!['bmap', 'tmap', 'gmap'].includes(from) || !['bmap', 'tmap', 'gmap'].includes(to)) {
     throwError(paramerrorings['from']);
     throwError(paramerrorings['to']);
     return false
   }
 
   // 参数值不等验证
-  if ( from === to ) {
+  if (from === to) {
     throwError('传入的初始坐标目标坐标类型不能相等')
     return false
   }
-  
+
   // 参数值合法验证
-  if ( !(from === 'bmap' && ['tmap', 'gmap'].includes(to)) || !(['tmap', 'gmap'].includes(from) && to === 'bmap') ) {
+  if (!(from === 'bmap' && ['tmap', 'gmap'].includes(to)) || !(['tmap', 'gmap'].includes(from) && to === 'bmap')) {
     throwError('传入的初始坐标目标坐标类型冲突，请检查。gmap和tmap不能相互转换，因为腾讯和高德是同一个坐标系，不需要转换')
     return false
   }
@@ -99,7 +106,7 @@ const paramerrorings = {
  * 抛出错误信息
  * @param 错误信息 errText string
  */
-function throwError(errText) {
+function throwError(errText: string) {
   console.error(errText)
 }
 
@@ -121,16 +128,19 @@ function throwError(errText) {
  })
  ```
  */
-function transCoordinate(params) {
+function transCoordinate(params: CoordinateParams) {
   const { from, to, lng, lat } = params
   let result = {
-    lat: '',
-    lng: ''
+    lat: 0,
+    lng: 0
   }
-  if ( from === 'bmap' && ['tmap', 'gmap'].indexOf(to) > -1 ) {
+  if (!verifyParams(params)) {
+    return result
+  }
+  if (from === 'bmap' && ['tmap', 'gmap'].indexOf(to) > -1) {
     result = transBMapToTMap(lng, lat)
   }
-  else if ( ['tmap', 'gmap'].indexOf(from) > -1 && to === 'bmap' ) {
+  else if (['tmap', 'gmap'].indexOf(from) > -1 && to === 'bmap') {
     result = transTMapToBMap(lng, lat)
   }
   return result
